78455ac6564764e16e6db8767e616ef75a8ebda6
Nico <nicolas.rouge777@gmail.com>
2021-04-21 23:16:57 +0100

    sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignOut from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
   <div>
@@ -14,9 +15,12 @@ function App() {
   return (  
     <div>
       <Header/>
+
       <Switch>
       < Route exact path='/' component ={HomePage} />
       < Route path='/shop' component ={ShopPage} />
+      < Route path='/signin' component ={SignInAndSignOut} />
+
       </Switch>
     </div>
   );
---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.conponent';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignOut = () => (
+    <div className='sign-in-and-sign-up'><SignIn /></div>
+)
+
+export default SignInAndSignOut;
