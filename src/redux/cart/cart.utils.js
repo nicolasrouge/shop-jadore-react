551031af51bfd5815fadc5b81830e8f09812e4c3
Nico <nicolas.rouge777@gmail.com>
2021-05-01 19:24:43 +0100

    cart item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,16 +1,15 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.Id === cartItemToAdd.Id
+      cartItem => cartItem.id === cartItemToAdd.id
     );
-
-    if(existingCartItem){
-        return cartItems.map(cartItem => 
-            cartItem.id == cartItemToAdd.id
-            ?  { ...cartItem, quantity: cartItem.quantity + 1}
-            : cartItem
-        )
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
     }
-
-    return [...cartItems, {...cartItemToAdd, quantity:1}]
-};
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
